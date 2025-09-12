f2d68e8058af6d404e16050659b53d4a9dc5dd7c
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-12 12:00:12 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+//  hello(){
+//  int sum,a,b;
+//  printf("enter num: ");
+//  scanf("%d",&a);
+//  printf("enter second num: ");
+//  scanf("%d",&b);
+//  sum=a+b;
+//  printf("sum =%d",sum);
+//  return 0;
+   //}
+
+int hello(a,b) {
+
+printf("%d ",a);
+printf("%d",b);
+return 0;
+
+};
+
+int main(){
+    int a;
+    int b;
+
+    printf("enter num: ");
+scanf("%d",&a);
+printf("enter num: ");
+scanf("%d",&b);
+
+hello(a,b); 
+
+
+
+
+
+}
+
+
