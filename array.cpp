f14fc3d15afe70694a81de3cc82363e56f772143
Bhagirath2120 <bhagirathr2005@gmail.com>
2025-09-13 15:49:01 +0530

    array

---
+++ array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+      int a[5];
+      int s=0,i;
+
+      cout<<"address of array\n ";
+
+for(i=0;i<=5;i++){
+   
+  cout<<  sizeof(a[5])<<endl;
+
+}
+     cout<< "enter 5 ints:\n ";
+           for(i=1;i<=5;i++){  
+        
+            cin>>a[5];
+            cout<<"                    ";
+            cout<<"                ";
+            
+      }
+     
+
+      s=s+a[i];
+
+    cout<<"\n"<< s <<endl;
+
+
+return 0;
+}
