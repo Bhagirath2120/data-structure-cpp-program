34b7928ffb1c3519585202ab6abeb60fb0cfdea4
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-14 19:10:10 +0530

    class

---
+++ reverse-array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+    int i;
+    int a[5];
+cout<<"Enter 5 ints\n";
+for(i=0;i<5;i++){
+    cin>>a[i];
+    for(i=0;i<5;i++){
+        cout<<endl<<i<<a[i]<<i<<a[i];
+    }
+    getch();
+
+}
+cout<<endl<<endl;
+for(i=4;i>0;i--){
+    cout<<i<<a[i];
+    getch();
+}
+
+
+
+return 0;
+
+
+}
