bf0e21095ce0cec636aa3217a2c818f8a921139e
Bhagirath2120 <bhagirathr2005@gami.com>
2025-09-17 21:38:19 +0530

    array

---
+++ array-update-operation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+ int arr[5];
+ int i,n;
+ for(i=0;i<5;i++)
+ cin>>arr[i];
+ cout<<"Enter position to update:";
+ cin>>n;
+ cout<<"Enter New Element:";
+ cin>>arr[n];
+ for(i=0;i<5;i++)
+ cout<<arr[i]<<endl;
+
+ getch();
+return 0;
+
+}
