b3f5271c4f1861895f90c907f05c8c378bd4497c
Bhagirath2120 <bhagirathr2005@gami.com>
2025-10-06 21:58:28 +0530

    incomplete

---
+++ char-array.cpp
@@ -0,0 +1,21 @@
+//incomplete
+
+#include<iostream>
+using namespace std;
+
+int main(){
+string arr[5];
+
+for(int j=1;j<6;j++){
+cout<<"Enter "<<j<< " Name:-";
+// 
+cin>> arr[j];
+    
+}
+
+for(int i=0;i>5;i++){
+    cout<<arr[i];
+}
+
+
+}
