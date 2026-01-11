37b9e2cdac44b5447dd60f452d7c741dec3f0ace
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-11 16:45:35 +0530

    Reverse Digit

---
+++ reverseDigit.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter a number : ";
+    cin>> n;
+     int rev = 0;
+
+
+    //  while(n>0){
+    //      rev = (rev * 10)+ (n % 10);
+    //      n /= 10;
+    //  }
+
+
+   // for loop
+
+   for(; n>0; n/=10){
+
+    rev = (rev * 10) + ( n % 10);
+
+   }
+     
+   
+   cout<<rev;
+
+}
