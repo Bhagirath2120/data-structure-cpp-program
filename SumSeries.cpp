90af42492c4b1e3a547a6eaf9345466c296614bf
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-11 16:43:54 +0530

    Sum series

---
+++ SumSeries.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+ 
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter Number: ";
+    cin>> n;
+
+    int ans =0;
+    int test =1;
+
+
+    for loop
+    for( int i = 0; i <= n; i++){
+        if (i % 2 == 0){
+            ans -= i;
+        }
+          else{
+                 ans+=i;
+        }
+
+    }
+
+    // while loop
+
+    // while(n>=test){
+    //     if(n % 2 == 0){
+    //         ans -= n;
+    //     }
+    //     else{
+    //         ans += n;
+    //     }
+    //     n--;
+    // }
+    cout<< ans;
+}
