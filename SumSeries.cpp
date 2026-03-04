470e8ae8412f6c63492ba9e09d21c0c3320e7cc7
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-03-04 21:57:10 +0530

    sum series

--- SumSeries.cpp
+++ SumSeries.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main(){
 
     int n;
-    cout<<" Enter Number: ";
+     cout<<" Enter Number: ";
     cin>> n;
 
     int ans =0;
     int test =1;
 
 
-    for loop
+  //  for loop
     for( int i = 0; i <= n; i++){
         if (i % 2 == 0){
             ans -= i;
